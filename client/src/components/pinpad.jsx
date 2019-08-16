1d1d3de3296f4c3bc217aa2e0a1cc93249f8c257
Garrett Watson <garrettbauer@gmail.com>
2019-08-16 02:18:03 +0000

    Render basic pinpad

--- client/src/components/pinpad.jsx
+++ client/src/components/pinpad.jsx
@@ -1,33 +1,37 @@
-import React from 'react';\
+import React from 'react';
 
-class Pad extends React.component() {
+class Pinpad extends React.Component {
   constructor() {
     super();
     this.state = {
 
-    }
+    };
   }
 
   render() {
     return (
       <div style={{ textAlign: 'center' }}>
-        <table>
+        <table align="center" width="500">
           <tbody>
             {
-              [0, 1, 2].map((row) => {
+              [0, 1, 2].map((row) => (
                 <tr>
                   {
-                    [1, 2, 3].map((cell) => {
-                      <td></td>
-                    });
+                    [1, 2, 3].map((cell) => <td align="center"><h2>{cell + (3 * row)}</h2></td>)
                   }
                 </tr>
-              })
+              ))
             }
-            <tr><td>10</td></tr>
+            <tr>
+              <td />
+              <td><h2>10</h2></td>
+              <td />
+            </tr>
           </tbody>
         </table>
       </div>
     );
   }
 }
+
+export default Pinpad;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pinpad from './components/pinpad.jsx';
 
-const App = () => <div>Hi</div>;
+const App = () => <div><Pinpad /></div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
