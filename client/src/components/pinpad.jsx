e919c1b7bb91009c9d6e5a2ee8abfdee4fc9034e
Garrett Watson <garrettbauer@gmail.com>
2019-08-16 03:50:47 +0000

    Break pinPad table down into padRow, padButton components, add click handler and state to track clicks

--- client/src/components/pinpad.jsx
+++ client/src/components/pinpad.jsx
@@ -1,31 +1,47 @@
 import React from 'react';
+import PadButton from './padButton.jsx';
+import PadRow from './padRow.jsx';
 
 class Pinpad extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      clickedCells: {
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false,
+        7: false,
+        8: false,
+        9: false,
+        10: false,
+      },
     };
+
+    this.toggleCell = this.toggleCell.bind(this);
+  }
+
+  toggleCell(cell) {
+    console.log(cell);
+    const { clickedCells } = this.state;
+    clickedCells[cell] = !clickedCells[cell];
+    this.setState({ clickedCells });
   }
 
   render() {
     return (
       <div style={{ textAlign: 'center' }}>
-        <table align="center" width="500">
+        <table style={{ margin: '100px auto', width: 500 }}>
           <tbody>
             {
-              [0, 1, 2].map((row) => (
-                <tr>
-                  {
-                    [1, 2, 3].map((cell) => <td align="center"><h2>{cell + (3 * row)}</h2></td>)
-                  }
-                </tr>
-              ))
+              [0, 1, 2].map((row) => <PadRow key={`row${row}`} rowNumber={row} toggleCell={this.toggleCell} />)
             }
             <tr>
-              <td />
-              <td><h2>10</h2></td>
-              <td />
+              <PadButton />
+              <PadButton key="pad10" padNumber={10} toggleCell={this.toggleCell} />
+              <PadButton />
             </tr>
           </tbody>
         </table>
